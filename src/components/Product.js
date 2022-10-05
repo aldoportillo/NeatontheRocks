a7e588e67b3e2dd1cdaa86f6fb2bcf4497dd4fa5
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-05 15:43:32 -0500

    Add cart state to context and function to add to cart state

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {Context} from '../context/context'
 
 function Product(props){
 
@@ -7,16 +7,23 @@ function Product(props){
         height: "300px",
         width: "300px",
     }
+    const {addToCart} = React.useContext(Context)
+
+
     return(
         <div className="product-box">
             <img src={props.img} alt="" style={productImgStyles}/>
             <div className="info">
                 <h1>{props.name}</h1>
                 <h2>{props.price}</h2>
+                
+                <button onClick={()=>addToCart(props)}>Favorite this Item</button>
             </div>
             
         </div>
     )
+
+    
 }
 
 export default Product
--- src/context/context.js
+++ src/context/context.js
@@ -5,9 +5,33 @@ const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [allItems, setAllItems] = React.useState(productData)
+    const [cart, setCart] = React.useState([])
 
+    function toggleFavorite(id) {
+        const updatedArr = allItems.map(item => {
+            if (item.id === id){
+                return{
+                    ...item,
+                    isFavorited: !item.isFavorited,
+                }
+            }
+            return item
+        })
+        setAllItems(updatedArr)
+    }
+
+    function addToCart (obj) {
+        setCart(prevCart => {
+            return [
+                ...prevCart,
+                obj
+            ]
+        })
+    }
+
+    console.log(cart)
     return(
-        <Context.Provider value={{allItems}}>
+        <Context.Provider value={{allItems, toggleFavorite, addToCart}}>
             {children}
         </Context.Provider>
     )
