d21e65dd874a0800a8eda3b3ff78ee06af204383
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-05 17:03:01 -0500

    Add well alcohol, simple juice, and syrup data

---
+++ src/bar/alcoholData.js
@@ -0,0 +1,116 @@
+const alcoholData = {
+    whiskey: {
+        id: 1,
+        fat: 0,
+        carb: 0.3,
+        sugar: .03,
+        protein: 0,
+        calories: 71,
+        proof: 86,
+        ethanol: 10,
+    },
+    vodka: {
+        id: 2,
+        fat: 0,
+        carb: 0,
+        sugar: 0,
+        protein: 0,
+        calories: 64,
+        proof: 80,
+        ethanol: 9.14,
+    },
+    rum: {
+        id: 3,
+        fat: 0,
+        carb: 0,
+        sugar: 0,
+        protein: 0,
+        calories: 64,
+        proof: 80,
+        ethanol: 9.14,
+    },
+    gin: {
+        id: 4,
+        fat: 0,
+        carb: 0,
+        sugar: 0,
+        protein: 0,
+        calories: 64,
+        proof: 80,
+        ethanol: 9.14,
+    },
+    tripleSec: {
+        id: 4,
+        fat: 0,
+        carb: 7,
+        sugar: 7,
+        addedSugar: 3,
+        protein: 0,
+        calories: 95,
+        proof: 80,
+        ethanol: 9.5,
+    },
+}
+
+const juiceData = {
+    lime: {
+        id: 1,
+        fat: 0,
+        carb: 2.6,
+        sugar: 0.5,
+        addedSugar: 0,
+        protein: 0.1,
+        calories: 10.4,
+    },
+    lemon: {
+        id: 2,
+        fat: 0,
+        carb: 2.6,
+        sugar: 0.73,
+        addedSugar: 0,
+        protein: 0,
+        calories: 10.4,
+    },
+    cranberry: {
+        id: 3,
+        fat: 0.03,
+        carb: 4.27,
+        sugar: 3.75,
+        addedSugar: 3.75,
+        protein: 0,
+        calories: 17,
+    },
+    pineapple: {
+        id: 4,
+        fat: 0.04,
+        carb: 4.02,
+        sugar: 3.11,
+        addedSugar: 0,
+        protein: 0,
+        calories: 17,
+    },
+    orange: {
+        id: 5,
+        fat: 0.06,
+        carb: 3.22,
+        sugar: 2.6,
+        addedSugar: 0,
+        protein: 0,
+        calories: 14,
+    }
+    
+}
+
+const syrupData = {
+    simple: {
+        id: 1,
+        fat: 0,
+        carb: 0,
+        sugar: 13,
+        addedSugar: 13,
+        protein: 0,
+        calories: 52,
+    }
+}
+
+export {alcoholData, juiceData, syrupData}
