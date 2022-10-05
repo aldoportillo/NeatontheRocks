5dd8520f7dbbbfda0faafbc21558556497cb2a3e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-05 00:15:27 -0500

    Import product images and create data

---
+++ src/products/productData.js
@@ -0,0 +1,11 @@
+
+export const productData = [
+    {id: 1, name: "18 oz Tin", price: 2.58, isFavorited: false, description: "", img: require('./tools/18oz-tin.webp')},
+    {id: 2, name: "28 oz Tin", price: 2.75, isFavorited: false, description: "", img: require('./tools/28oz-tin.webp')},
+    {id: 3, name: "Bar Spoon", price: 7.95, isFavorited: false, description: "", img: require('./tools/bar-spoon.webp')},
+    {id: 4, name: "Beaker Jigger", price: 4.99, isFavorited: false, description: "", img: require('./tools/beaker-jigger.webp')},
+    {id: 5, name: "Corkscrew", price: 12.58, isFavorited: false, description: "", img: require('./tools/corkscrew.webp')},
+    {id: 6, name: "Mixing Glass", price: 11.95, isFavorited: false, description: "", img: require('./tools/mixing-glass.webp')},
+    {id: 7, name: "Peeler", price: 3.99, isFavorited: false, description: "", img: require('./tools/peeler.webp')},
+    {id: 7, name: "Liquid Intelegence", price: 99.99, isFavorited: false, description: "", img: require('./tools/book.jpeg')},
+]
