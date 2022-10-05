b90041f30b57543d7bbad516d8c30d52b49ffab2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-05 00:16:24 -0500

    Wrap app component in Context Provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { ContextProvider } from './context/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </ContextProvider>
   </React.StrictMode>
 );
 
