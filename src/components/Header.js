6c72a4944b69ee5b7c267b35415042ab592934e6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-04 23:37:09 -0500

    Style header and add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom'
 export default function Header () {
     return (
         <div className='header'>
-            <Link to="/"><h1>Neat on the Rocks</h1></Link>
-            <Link to="/products"><h3>Products</h3></Link>
-            <Link to="/cart"><h3>Cart</h3></Link>
-            <Link to="/macro"><h3>Nutrition</h3></Link>
+            <div className='title'>
+                <img require src={require('../media/logo.png')} alt=""/> 
+                <Link to="/"><h1>Neat on the Rocks</h1></Link>
+            </div>
+            
+            <nav>
+                <Link to="/products"><h3>Products</h3></Link>
+                <Link to="/cart"><h3>Cart</h3></Link>
+                <Link to="/macro"><h3>Nutrition</h3></Link>
+            </nav>
             
         </div>
         
