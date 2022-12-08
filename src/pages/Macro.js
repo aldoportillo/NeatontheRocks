295db9cbe4934331e7e602bcc68508fd27e514f7
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-08 13:46:21 -0600

    Call function from macros.js

--- src/pages/Macro.js
+++ src/pages/Macro.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { getMacros } from '../bar/macros'
 import LiquidForm from '../components/LiquidForm'
 import NutritionLabel from '../components/NutritionLabel'
 import { Context } from '../context/context'
+import { alcoholData } from '../bar/alcoholData'
 
 
 export default function Macro () {
@@ -11,7 +13,9 @@ export default function Macro () {
     const [cocktail, setCocktail] = React.useState([])
     const [forms, setForms] = React.useState([1])
 
+    const array = [{name: 'vodka', ounces: '2'}, {name: 'simple', ounces: '2'}, {name: 'triple sec', ounces: '2'}]
     console.log(cocktail);
+    console.log(getMacros(array, alcoholData))
 
     function addForm() {
         setForms([...forms, 1])
