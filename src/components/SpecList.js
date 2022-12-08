cd957376d2952702f67bb3961a723b70592be330
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-08 17:32:08 -0600

    Create spec-list component

---
+++ src/components/SpecList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SpecList({name, ounces}) {
+  return (
+    <div className='spec-list'>
+        <h3>{name}</h3>
+        <h3>{ounces}</h3>
+    </div>
+  )
+}
