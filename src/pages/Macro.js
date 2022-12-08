fe98a544ab29012ada2784c4e6189f2b53c6fed0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-08 17:36:53 -0600

    Render ingidient list

--- src/pages/Macro.js
+++ src/pages/Macro.js
@@ -4,6 +4,7 @@ import LiquidForm from '../components/LiquidForm'
 import NutritionLabel from '../components/NutritionLabel'
 import { Context } from '../context/context'
 import { alcoholData } from '../bar/alcoholData'
+import SpecList from '../components/SpecList'
 
 
 export default function Macro () {
@@ -11,13 +12,18 @@ export default function Macro () {
     const {nutrition} = React.useContext(Context)
 
     const [cocktail, setCocktail] = React.useState([])
-   
+
+   const renderSpecList = cocktail.map(item => {
+    return <SpecList name={item.name} ounces={item.ounces} />
+   })
     return (
         <div className='nutrition-page'> 
             <div className='form'>
             <LiquidForm setCocktail={setCocktail} cocktail={cocktail}/>
             </div>
-            
+            <div className='spec-list-container'>
+                {renderSpecList}
+            </div>
             <div className='nutrition-label'>
                <NutritionLabel item={getMacros(cocktail, alcoholData)}/>
             </div> 
