5dac0aad28f6848081df68c5a5ed6d1c95f60170
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-08 11:52:03 -0600

    Render liquids by mapping through liquid array

--- src/pages/Macro.js
+++ src/pages/Macro.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import NutritionLabel from '../components/NutritionLabel'
 import { Context } from '../context/context'
+import { arrLiquids } from '../bar/alcoholData'
 
 export default function Macro () {
 
     const {nutrition} = React.useContext(Context)
 
+    console.log(arrLiquids)
+
+    const renderOptions = arrLiquids.map(liquid => {
+        return <option value={liquid.name}>{liquid.name}</option>
+    })
+
     return (
         <div className='nutrition-page'> 
-            <div className='nutrition-label'>
-               <NutritionLabel item={nutrition}/>
 
-            </div>
+            <form>
+                <select name="liquid">
+                    {renderOptions}
+                </select>
+                <input placeholder='Please Enter Amount in OZ'/>
+            </form>
+            {/* <div className='nutrition-label'>
+               <NutritionLabel item={nutrition}/>
+            </div> */}
 
         </div>
     )
