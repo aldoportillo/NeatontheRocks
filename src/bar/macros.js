2ffc0a60f7192a3594950dab736771163772cc08
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-08 14:07:30 -0600

    Finish function to return an object of macros

--- src/bar/macros.js
+++ src/bar/macros.js
@@ -34,19 +34,29 @@ export const getMacros = (arr, arr2) => {
     fat: 0,
     carb: 0,
     sugar: 0,
+    addedSugar: 0,
     protein: 0,
     calories: 0,
     ethanol: 0,
   }
 
-  return arr.map((item, index) => {
-    arr2.map((item2, index) => {
+  arr.map((item) => {
+    arr2.map((item2) => {
       if(item2.name === item.name){
-        console.log(item2.calories)
+        macros.calories += item.ounces * item2.calories
+        macros.protein += item.ounces * item2.protein
+        macros.addedSugar += item.ounces * item2.addedSugar
+        macros.sugar += item.ounces * item2.sugar
+        macros.carb += item.ounces * item2.carb
+        macros.fat += item.ounces * item2.fat
       }
+      return null
+
     })
-    return item.name + index
+    return null
   })
+
+  return macros
   
 
 }
