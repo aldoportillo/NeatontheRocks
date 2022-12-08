e24ba368108773ba160ca6acbf732a95dff6f81d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-08 13:45:43 -0600

    Get ethanol of items in array using alcohol data

--- src/bar/macros.js
+++ src/bar/macros.js
@@ -1,3 +1,5 @@
+import { alcoholData } from '../bar/alcoholData'
+
 const macros = [4,20,3,20];
       //Gainging Calories from Grams
 const fatCal = (gramFat) => {
@@ -16,36 +18,6 @@ const ethanolCal = (gramEthanol) => {
   return gramEthanol * 7;
 };
 
-      //Gaining Gram from Calories
-
-const fatGram = (calFat) => {
-  return calFat / 9;
-};
-
-const carbGram = (calCarb) => {
-  return calCarb / 4;
-};
-
-const proteinGram = (calProtein) => {
-  return calProtein / 4;
-};
-
-const ethanolGram = (calEthanol) => {
-  return calEthanol / 7;
-};
-
-//use a .map iterator method
-/*const Calories = macros.map(number => {
-  if (number === macros.indexOf[0]){
-    return fatCal(macros[0]);
-  }
-});
-
-console.log(macros);
-
-console.log(macros.indexOf(1));
-*/
-
 const calories = [];
 
 calories.push(fatCal(macros[0]), carbCal(macros[1]), proteinCal([2]),ethanolCal([3]));
@@ -53,45 +25,30 @@ calories.push(fatCal(macros[0]), carbCal(macros[1]), proteinCal([2]),ethanolCal(
 //find a way to sum up all items in an array
 
 const totCal = calories[0] + calories[1] + calories[2] + calories[3];
-console.log(`The calories of your drink cointaining ${macros[0]} grams of fat, ${macros[1]} grams of carb, ${macros[2]} grams of protein, and ${macros[3]} grams of ethanol contains ${totCal} calories!`);
+//console.log(`The calories of your drink cointaining ${macros[0]} grams of fat, ${macros[1]} grams of carb, ${macros[2]} grams of protein, and ${macros[3]} grams of ethanol contains ${totCal} calories!`);
 
 
-
-function countInArray(array, value) {
-  return array.reduce((n, x) => n + (x === value), 0);
-}
-console.log(countInArray('food', 'o'));
-
-//cool code i wrote
-const subLength = (word, letter) => {
-  if (word.split(letter).length-1 === 2){
-    return word.lastIndexOf(letter) - word.indexOf(letter) + 1;
-  } else {
-    return 0;
+export const getMacros = (arr, arr2) => {
+  console.log("you got this")
+  const macros ={
+    fat: 0,
+    carb: 0,
+    sugar: 0,
+    protein: 0,
+    calories: 0,
+    ethanol: 0,
   }
-}
 
-
-const groceries = list => {
-  let listString = ''
-
-  for (let i=0; i<list.length; i++) {
-    listString += list[i].item;
-    if (i < list.length - 2) {
-      listString += ', ';
-    } else if (i == list.length - 2){
-      listString += ' and ';
-    }
-  }
+  return arr.map((item, index) => {
+    arr2.map((item2, index) => {
+      if(item2.name === item.name){
+        console.log(item2.calories)
+      }
+    })
+    return item.name + index
+  })
   
-  return listString;
+
 }
 
-groceries( [{item: 'Carrots'}, {item: 'Hummus'}, {item: 'Pesto'}, {item: 'Rigatoni'}] );
-// returns 'Carrots, Hummus, Pesto and Rigatoni'
- 
-groceries( [{item: 'Bread'}, {item: 'Butter'}] );
-// returns 'Bread and Butter'
- 
-groceries( [{item: 'Cheese Balls'}] );
-// returns 'Cheese Balls'
+
