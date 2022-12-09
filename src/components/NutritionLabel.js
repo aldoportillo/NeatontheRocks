81269045540b60bb8be3a5c2ba7e6a814465c64e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-08 19:35:59 -0600

    Mimmick FDA label

--- src/components/NutritionLabel.js
+++ src/components/NutritionLabel.js
@@ -3,14 +3,23 @@ import React from "react";
 function NutritionLabel ({item}) {
     return(
         <div className="label-container">
-            <h2>Calories: {item.calories}</h2>
-            <h2>Carbs: {item.carb}</h2>
-            <h2>Sugar: {item.sugar}</h2>
-            <h2>AddedSugar: {item.addedSugar}</h2>
-            <h2>Fat: {item.fat}</h2>
-            <h2>Ethanol: {item.ethanol}</h2>
-            <h2>Protein: {item.protein}</h2>
-            
+            <h2 className="title">Cocktail Facts</h2>
+            <div className="thick1"></div>
+            <h3>Serving Size <p>1 drink</p></h3>
+            <h2>Calories <p>{item.calories}</p></h2>
+            <div className="thick2"></div>
+            <h3>Ethanol  <p>{item.ethanol}g </p></h3>
+            <hr />
+            <h3>Total Fat  <p>{item.fat} g</p></h3>
+            <hr />
+            <h3>Total Carbohydrate  <p>{item.carb} g</p></h3>
+            <hr />
+            <h3>    <p>Total Sugars  {item.sugar} g</p></h3>
+            <hr />
+            <h3>        <p>Includes  {item.addedSugar} g Added Sugars</p></h3>
+            <hr />
+            <h3>Protein  <p>{item.protein} g</p></h3>
+            <div className="thick1"></div>
         </div>
         
 
