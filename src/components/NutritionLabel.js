3310b047555e7314973fe3570929d770bb26821c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-09 12:39:27 -0600

    Round the decimals up

--- src/components/NutritionLabel.js
+++ src/components/NutritionLabel.js
@@ -6,19 +6,19 @@ function NutritionLabel ({item}) {
             <h2 className="title">Cocktail Facts</h2>
             <div className="thick1"></div>
             <h3>Serving Size <p>1 drink</p></h3>
-            <h2>Calories <p>{item.calories}</p></h2>
+            <h2>Calories <p>{Math.ceil(item.calories)}</p></h2>
             <div className="thick2"></div>
-            <h3>Ethanol  <p>{item.ethanol}g </p></h3>
+            <h3>Ethanol  <p>{Math.ceil(item.ethanol)}g </p></h3>
             <hr />
-            <h3>Total Fat  <p>{item.fat} g</p></h3>
+            <h3>Total Fat  <p>{Math.ceil(item.fat)} g</p></h3>
             <hr />
-            <h3>Total Carbohydrate  <p>{item.carb} g</p></h3>
+            <h3>Total Carbohydrate  <p>{Math.ceil(item.carb)} g</p></h3>
             <hr />
-            <h3>    <p>Total Sugars  {item.sugar} g</p></h3>
+            <h3>    <p>Total Sugars  {Math.ceil(item.sugar)} g</p></h3>
             <hr />
-            <h3>        <p>Includes  {item.addedSugar} g Added Sugars</p></h3>
+            <h3>        <p>Includes  {Math.ceil(item.addedSugar)} g Added Sugars</p></h3>
             <hr />
-            <h3>Protein  <p>{item.protein} g</p></h3>
+            <h3>Protein  <p>{Math.ceil(item.protein)} g</p></h3>
             <div className="thick1"></div>
         </div>
         
