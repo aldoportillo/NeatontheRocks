311b251dd4fb1490dc42be9d47d0b955671cf133
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-09 12:57:05 -0600

    Restructure nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,9 @@ export default function Header () {
             </div>
             
             <nav>
+                <Link to="/macro"><h3>Nutrition</h3></Link>
                 <Link to="/products"><h3>Products</h3></Link>
                 <Link to="/cart"><h3>Cart</h3></Link>
-                <Link to="/macro"><h3>Nutrition</h3></Link>
             </nav>
             
         </div>
