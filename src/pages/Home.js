651d434f17a680bbc4b2920a133c6c394f6d3fc6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-10 11:52:35 -0600

    Edit Homepage text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,16 @@ export default function Home () {
             <div className='paragraph'>
                 <div className='text-content'>
                 <h2>Prohibition and the FDA</h2>
-                    <p>In 1935, The Alcohol Administration Act (now the TTB) was created to generate tax revenue from alcohol after the end of Prohibition. (1920-1935) The TBB was also responsible for regulating alcohol labels.</p>
-                    <p>A problem arose in 1990, the FDA required mandatory Nutrition Facts labels. Unfortunately, alcohol was regulated by the TTB, so they were exempt. At the time the TTB just required alcohol percentage except for beer until 1995 when beer won their right to display alcohol percentage as well.</p>
-                </div>
+                    <p>In 1935, The Alcohol Administration Act <i>(now the TTB)</i> was created to generate tax revenue from alcohol after the end of Prohibition. <i>(1920-1935)</i> The TBB was also responsible for regulating alcohol labels.</p>
+                    <p>A problem arose in 1990, the FDA required mandatory Nutrition Facts labels. Unfortunately, alcohol was regulated by the TTB, so alcohol was exempt. The only requirement for alcohol was to display their alcohol percentage.</p>
+                 </div>
                 <img src={require('../bar/cocktails/hazelnutOldFashioned.jpg')} alt="hazelnut old fashioned" />
             </div>
             <br />
             <div className='paragraph'>
                 <div className='text-content'>
                     <h2>Fight for labels</h2>
-                    <p>Multiple agencies have attempted to add the label to alcohol such as the Center for science in the public interest the TTB said that putting the nutrition facts on label would make people think its nutritious</p>
+                    <p>Multiple agencies have attempted to add the Nutrition Facts label. One time in particular, The Center for Science in the Public Interest attempted to file a motion to display the label on alcohol. The TTB responded to the motion by saying that adding a <i>Nutrition</i> Facts label will misrepresent the alcohol as being Nutritious. </p>
                 </div>
                 <img src={require('../bar/cocktails/newYorkSour.jpg')} alt="new york sour" />
             </div>
@@ -23,13 +23,10 @@ export default function Home () {
             <div className='paragraph'>
                 <div className='text-content'>
                     <h2>The Mission</h2>
-                    <p>Neat on the Rocks was created because I have always enjoyed a drink but more importantly I have taken my health serious. I always wished there was a calculator that would tell me exactly what I am drinking, so I made it. </p>
+                    <p>Neat on the Rocks was created to give the general public an idea of the macronutrients they are consuming when they drink a cocktail. I have a background in bartending, chemistry and nutrition. I found this as an opportunity to incorporate my passions into a project. This is just the beginning. I’m currently working on a dilution calculator that ensures the final product is always the perfect drink. Further releases will contain a cocktail page that contains some of my specialty cocktails and an e-commerce section to buy everything needed to get started on your journey to becoming the best bartender or party host. </p>
                 </div>
             <img src={require('../bar/cocktails/cadillacMargarita.jpg')} alt="hazelnut old fashioned" />
             </div>
-            <br />
-            <h2>Disclaimer</h2>
-            <p>Alcohol Facts are still difficult to find, and I have made a database with some of the most common spirits, cordials, and mixers. There could be a slight uncertainty when it comes to some of the macro nutrients, but I am certain this is as close as it gets. Thank you for taking your time to visit the site, and Cheers!</p>
         </div>
     )
 }
