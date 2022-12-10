823e0cda40027edace4c904931822f27b1f333b6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-10 12:55:47 -0600

    Add links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer'>
-        <h1>Footer</h1>
+        <div className='footer-rows'>
+            <a href="https://www.aldoportillo.com/" target="_blank" rel="noreferrer">Checkout my Portfolio</a>
+            <a href="https://github.com/aldoportillo" target="_blank" rel="noreferrer">Follow me on Github</a>
+        </div>
+        <div className='footer-rows'>
+            <a href='https://whimsical.com/neat-on-the-rocks-XNT8xwpnk6JWGdoByfL5cp' target="_blank" rel="noreferrer">Sitemap</a>
+            <p>Aldo Portillo &copy; 2022</p> 
+        </div>
     </div>
   )
 }
