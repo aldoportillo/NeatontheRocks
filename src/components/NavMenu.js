af4b7a6c79c28ff1658326a01d608a91ea6bb61a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-10 13:38:29 -0600

    Create Nav menu component

---
+++ src/components/NavMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NavMenu() {
+  return (
+    <nav>
+                <Link to="/macro"><h3>Nutrition</h3></Link>
+                <Link to="/products"><h3>Products</h3></Link>
+                <Link to="/cart"><h3>Cart</h3></Link>
+    </nav>
+  )
+}
