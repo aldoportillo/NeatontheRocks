ce718b08b5693b39e43ddd8cfc57659ed4e04632
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-10 13:01:32 -0600

    Create PageContainer Component and wrap routes in it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import Header from "./components/Header";
 import {Routes, Route} from "react-router-dom"
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Macro from "./pages/Macro";
-import Footer from "./components/Footer";
+import PageContainer from "./components/PageContainer";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/macro" element={<Macro />} />
+        <Route exact path="/" element={<PageContainer children={<Home />}/>} />
+        <Route path="/products" element={<PageContainer children={<Products />}/>} />
+        <Route path="/cart" element={<PageContainer children={<Cart />}/>} />
+        <Route path="/macro" element={<PageContainer children={<Macro />}/>} />
       </Routes>
-
-      <Footer />
     </div>
     
   );
