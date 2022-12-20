846325a4de10676c0c53a8b1d3ebdd1b62aa1aad
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-20 01:34:17 -0600

    Render cocktail url with id params

--- src/pages/Cocktails.js
+++ src/pages/Cocktails.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default function Cocktails() {
 
@@ -15,30 +16,24 @@ export default function Cocktails() {
       })
     }, [])
 
-    //Folllowing code is kinda for cocktail page
+    const renderCocktails = cocktailData.map(cocktail => {
+        const {name, _id} = cocktail
 
-
-    const {name, specs, instructions, description} = cocktailData[0];
-
-    const renderSpecList = specs.map(liquid => { //Remember to take in account bitters and any odd ball add condtional if has dashes if has ounces
-
-        return <li> <h4>{liquid.spirit}</h4> <h5>{liquid.ounces}</h5> OZ </li> //For ounces
-
+        return (
+            <div className='cocktail-card'>
+                <Link to={`/cocktail/${_id}`}>
+                    <h3>{name}</h3>
+                </Link>
+            </div>
+        )
     })
 
-    const renderInstructions = instructions.map(step => { //Remember to take in account bitters and any odd ball add condtional if has dashes if has ounces
-
-        return <li> <h4>{step} </h4></li> //For ounces
-
-    })
+    
   return (
-    // The Following Code is Actually for the Cocktail page component
-    <div className='cocktail-page'>
-        <h2>{name}</h2>
-        <div className='img'></div>
-        <ul>{renderSpecList}</ul>
-        <ol>{renderInstructions}</ol>
-        <h5>{description}</h5>
+    <div className='cocktails-page'>
+        <div className='cocktail-list-container'>
+            {renderCocktails}
+        </div>
     </div>
   )
 }
