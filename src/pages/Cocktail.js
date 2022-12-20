52c5d6a27b1f93dc3f4063eb2373a4049df5318a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-20 15:36:16 -0600

    Pass macros from DB

--- src/pages/Cocktail.js
+++ src/pages/Cocktail.js
@@ -9,9 +9,7 @@ export default function Cocktail(props) {
     console.log("From cocktail page")
     console.log(data);
 
-    const {name, specs, instructions, description, image} = data;
-
-    const item = {calories: 2, ethanol: 3, fat:4, carb: 6, sugar: 6, addedSugar: 6, protein:5}
+    const {name, specs, instructions, description, image, macros} = data;
 
     const renderSpecList = specs.map(liquid => { 
         if(liquid.dashes) {
@@ -48,19 +46,19 @@ export default function Cocktail(props) {
             <h2 className="title">Cocktail Facts</h2>
             <div className="thick1"></div>
             <h3>Serving Size <p>1 drink</p></h3>
-            <h2>Calories <p>{Math.ceil(item.calories)}</p></h2>
+            <h2>Calories <p>{Math.ceil(macros.calories)}</p></h2>
             <div className="thick2"></div>
-            <h3>Ethanol  <p>{Math.ceil(item.ethanol)}g </p></h3>
+            <h3>Ethanol  <p>{Math.ceil(macros.ethanol)}g </p></h3>
             <hr />
-            <h3>Total Fat  <p>{Math.ceil(item.fat)} g</p></h3>
+            <h3>Total Fat  <p>{Math.ceil(macros.fat)} g</p></h3>
             <hr />
-            <h3>Total Carbohydrate  <p>{Math.ceil(item.carb)} g</p></h3>
+            <h3>Total Carbohydrate  <p>{Math.ceil(macros.carb)} g</p></h3>
             <hr />
-            <h3>    <p>Total Sugars  {Math.ceil(item.sugar)} g</p></h3>
+            <h3>    <p>Total Sugars  {Math.ceil(macros.sugar)} g</p></h3>
             <hr />
-            <h3>        <p>Includes  {Math.ceil(item.addedSugar)} g Added Sugars</p></h3>
+            <h3>        <p>Includes  {Math.ceil(macros.addedSugar)} g Added Sugars</p></h3>
             <hr />
-            <h3>Protein  <p>{Math.ceil(item.protein)} g</p></h3>
+            <h3>Protein  <p>{Math.ceil(macros.protein)} g</p></h3>
             <div className="thick1"></div>
         </div>
             </div>
