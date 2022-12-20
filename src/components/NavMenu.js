6cb4f65f4f4ce1888f668a1dc10b2ce6ecce281e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-20 13:27:32 -0600

    Add cocktails link to nav

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -5,8 +5,8 @@ export default function NavMenu() {
   return (
     <nav>
                 <Link to="/macro"><h3>Nutrition</h3></Link>
+                <Link to="/cocktails"><h3>Cocktails</h3></Link>
                 <Link to="/products"><h3>Products</h3></Link>
-                <Link to="/cart"><h3>Cart</h3></Link>
     </nav>
   )
 }
