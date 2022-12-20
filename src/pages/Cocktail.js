54ceeae3977cedeb21511c235ed681547010589b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-20 02:03:06 -0600

    Render cocktail page based off props passed through link

---
+++ src/pages/Cocktail.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+export default function Cocktail(props) {
+
+    const location = useLocation()
+    const { from, data } = location.state
+
+    console.log("From cocktail page")
+    console.log(data);
+
+    const {name, specs, instructions, description} = data;
+
+    const renderSpecList = specs.map(liquid => { //Remember to take in account bitters and any odd ball add condtional if has dashes if has ounces
+        return <li> <h4>{liquid.spirit}</h4> <h5>{liquid.ounces}</h5> OZ </li> //For ounces
+    })
+
+    const renderInstructions = instructions.map(step => { //Remember to take in account bitters and any odd ball add condtional if has dashes if has ounces
+        return <li> <h4>{step} </h4></li> //For ounces
+    })
+  return (
+    <div className='cocktail-page'>
+        <h2>{name}</h2>
+        <div className='img'></div>
+        <ul>{renderSpecList}</ul>
+        <ol>{renderInstructions}</ol>
+        <h5>{description}</h5> 
+    </div>
+  )
+}
