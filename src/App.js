6d609cbba572eb201f9ed67db88ba5c11e7e3dfc
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-22 17:03:15 -0600

    Call spirits api and pass data down to Macro page

--- src/App.js
+++ src/App.js
@@ -13,13 +13,21 @@ import axios from "axios";
 function App() {
 
   const [cocktailData, setCocktailData] = React.useState([])
+  const [spiritData, setSpiritData] = React.useState([])
 
     React.useEffect(( ) => {
-        axios.get(`https://neatontherocks-server.onrender.com/api/cocktails`)
-      .then(res => {
-        const cocktailData = res.data;
-        setCocktailData(cocktailData)
-      })
+        axios
+          .get(`https://neatontherocks-server.onrender.com/api/cocktails`)
+          .then(res => {
+            const cocktailData = res.data;
+            setCocktailData(cocktailData)
+          })
+        axios
+          .get('http://localhost:5000/api/spirits')
+          .then(res => {
+            const spiritData = res.data;
+            setSpiritData(spiritData)
+          })
     }, [])
 
 
@@ -29,7 +37,7 @@ function App() {
         <Route exact path="/" element={<PageContainer children={<Home />}/>} />
         <Route path="/products" element={<PageContainer children={<Products />}/>} />
         <Route path="/cart" element={<PageContainer children={<Cart />}/>} />
-        <Route path="/macro" element={<PageContainer children={<Macro />}/>} />
+        <Route path="/macro" element={<PageContainer children={<Macro spiritData={spiritData}/>}/>} />
         <Route path="/cocktails" element={<PageContainer children={<Cocktails cocktailData={cocktailData}/>} />} />
         <Route path="/cocktail/:id" element={<PageContainer children={<Cocktail />} />} />
       </Routes>
