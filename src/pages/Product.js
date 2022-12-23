8e7b9d702e0ef550993de95c1e66fe7d5b95d842
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-23 11:51:43 -0600

    Create add to cart functionality to product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Link, useLocation, useParams } from 'react-router-dom'
+import { Context } from '../context/context'
 
 
 export default function Product() {
 
     const {id} = useParams()
     const location = useLocation()
+    const {addToCart} = React.useContext(Context)
 
-    const {name, price, description, isFavorited} = location.state.data
+    const {name, price, description, img} = location.state.data
 
   return (
     <div className='product-page'>
-        <div>Product {id}</div>
+        <div>Product</div>
         <h3>{name}</h3>
+        <img src={img.mainImage} alt=""/>
         <h3>{price}</h3>
         <h3>{description}</h3>
-        <h3>{isFavorited}</h3>
+        <button onClick={()=>addToCart(location.state.data)}>Add to Cart</button>
         <Link to="/products" ><button>Back to Products</button></Link>
     </div>
     
