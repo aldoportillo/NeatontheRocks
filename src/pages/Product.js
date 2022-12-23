0b66baa65894b28096a8d580d6f557fa71c10468
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-22 19:04:16 -0600

    Recieve state from link to render product

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 
 
 export default function Product() {
 
     const {id} = useParams()
+    const location = useLocation()
+
+    const {name, price, description, isFavorited} = location.state.data
+
   return (
-    <div>Product {id}</div>
+    <div className='product-page'>
+        <div>Product {id}</div>
+        <h3>{name}</h3>
+        <h3>{price}</h3>
+        <h3>{description}</h3>
+        <h3>{isFavorited}</h3>
+    </div>
+    
   )
 }
