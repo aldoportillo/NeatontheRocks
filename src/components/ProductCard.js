6292deafed4818871f8f9866a319d364b84aa65f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-23 12:32:10 -0600

    Style product page

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -18,8 +18,8 @@ function ProductCard({item}){
                 <h3>{item.name}</h3>
                 <h4> &#36; {item.price}</h4>
                 <div className="product-box-buttons">
-                    <button onClick={()=>addToCart(item)}>Add to Cart</button>
-                    <Link to={`/products/${item._id}`} state={{ from: "/products", data: item }}> <button>View Item</button></Link>
+                    <button onClick={()=>addToCart(item)} className="--btn-white">Add to Cart</button>
+                    <Link to={`/products/${item._id}`} state={{ from: "/products", data: item }}> <button className="--btn-black">View Item</button></Link>
                 </div>
             </div>
             
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link, useLocation, useParams } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Context } from '../context/context'
 
 
 export default function Product() {
 
-    const {id} = useParams()
     const location = useLocation()
     const {addToCart} = React.useContext(Context)
 
@@ -13,13 +12,22 @@ export default function Product() {
 
   return (
     <div className='product-page'>
-        <div>Product</div>
-        <h3>{name}</h3>
-        <img src={img.mainImage} alt=""/>
-        <h3>{price}</h3>
-        <h3>{description}</h3>
-        <button onClick={()=>addToCart(location.state.data)}>Add to Cart</button>
-        <Link to="/products" ><button>Back to Products</button></Link>
+        <div className='product-left-page'>
+            <h2>{name}</h2>
+            <img src={img.mainImage} alt=""/>
+        </div>
+        <div className='product-right-page'>
+            <h3> &#36;{price}</h3>
+            <div className='description'>
+                <h3>Description</h3>
+                <p>{description}</p>
+            </div>
+            <div className='button-container'>
+                <button className="--btn-white" onClick={()=>addToCart(location.state.data)}>Add to Cart</button>
+                <Link to="/products"  ><button className="--btn-black">Back to Products</button></Link>
+            </div>
+        </div>
+        
     </div>
     
   )
