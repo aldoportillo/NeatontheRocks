1403df8763c406736903ba0c740d038a56523c35
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-24 17:05:35 -0600

    Pass loading stata and useEffect to rerender component when it changes

--- src/pages/Cocktails.js
+++ src/pages/Cocktails.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Cocktails({cocktailData}) {
+export default function Cocktails({cocktailData, loading}) {
 
     
 
     const renderCocktails = cocktailData.map(cocktail => {
         const {name, _id, image} = cocktail
         return (
-            <>
-            {cocktailData ?
             <div key={_id} className='cocktail-card'>
                 <Link to={`/cocktail/${_id}`} state={{ from: "cocktail", data: cocktail}}>
                     <h3>{name}</h3>
                     <img src={image.filePath} alt="" />
                 </Link>
-            </div> : <img src={require("../assets/loading.gif")} alt=""/>}
-            </>
+            </div>
         )
     })
 
-    React.useEffect(() => {
-        console.log("rerender")
-       },[cocktailData])
+    React.useEffect(()=> {
+        console.log('Cocktail rerender');
+    }, [loading])
     
   return (
     <div className='cocktails-page'>
-        <div className='cocktail-list-container'>
-            {renderCocktails}
-        </div>
+            {loading ?
+            <img src={require("../assets/loading.gif")} alt=""/> :
+            <div className='cocktail-list-container'>
+                {renderCocktails}
+            </div>}
+        
     </div>
   )
 }
